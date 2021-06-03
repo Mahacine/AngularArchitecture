8036559e9d8a8983531cdb9965d4c5225b015091
Mahacine <ettahirimahacine@gmail.com>
2021-06-03 10:10:07 +0100

    [dev] mahacine : add basic modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+{ path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }, 
+{ path: 'public', loadChildren: () => import('./public/public.module').then(m => m.PublicModule) }, 
+{ path: 'feature', loadChildren: () => import('./feature/feature.module').then(m => m.FeatureModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
